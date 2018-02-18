4489199ba59a19322b5dabbd28075dcdedc58e6e
Oier Bravo Urtasun <oierbravo@gmail.com>
2018-02-18 18:44:05 +0100

    refactor and redirects

--- www/js/notak.js
+++ www/js/notak.js
@@ -2,35 +2,24 @@
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
-
-  function note2MIDInumber(note) {
-    var noteValue = {
-      A:9,
-      B:11,
-      C:0,
-      D:2,
-      E:4,
-      F:5,
-      G:7
-    }
-    var output;
-    if(note.length == 2){
-      output = 12 + parseInt(noteValue[note.charAt(0)]) + 12 * parseInt(note.charAt(1));
-    } else if(note.length == 3) {
-      output = 12 + parseInt(noteValue[note.charAt(0)]) + 1 + 12 * parseInt(note.charAt(2));
-    } else {
-        output = '@#';
-    }
-    return output;
-  }
   $(document).ready(function(){
+    var norabidea = "ireki";
+    var keyboardMode = "triki";
     $(document).keydown(function(event){
       var keycode = event.keyCode;
-      console.log(keycode);
-      var nota = _.find(notakMap['keyboard'],{'KeyCode':keycode});
+      if(event.shiftKey){
+        norabidea = "ireki";
+      } else {
+        norabidea = "itxi";
+      }
+      //console.log(keycode);
+      var nota = _.find(keyboardMap[keyboardMode],{'KeyCode':keycode});
+      if(!nota){
+        nota = _.find(keyboardMap.options,{'KeyCode':keycode});
+      }
       if(nota){
         if(typeof nota.Zenbakia == 'string') {
-        
+
           $('.btn-' + nota.Zenbakia).trigger('click');
         } else {
           $('.triki-nota[data-zenbakia=' + nota.Zenbakia + ']').trigger('mousedown').addClass("circle-trigger");
@@ -41,7 +30,7 @@
       }
       }).keyup(function(event){
         var keycode = event.keyCode;
-        var nota = _.find(notakMap['keyboard'],{'KeyCode':keycode});
+        var nota = _.find(keyboardMap[keyboardMode],{'KeyCode':keycode});
         if(nota){
           if(typeof nota.Zenbakia == 'string') {
 
@@ -58,7 +47,7 @@
 
     var afinazioa = "DoFa";
 
-    var norabidea = "ireki";
+
     var notakMap;
     var notaMapLoaded = false;
 
@@ -74,111 +63,90 @@
       pianoNone:'piano-none'
 
     }
+    var trikiPlayer = new TrikiPlayer();
+
+
 
     var trikiMouseDownEv = function(e){
       var nota = $(e.target);
+
+      //console.log(nota);
       nota.addClass('color-acive');
       var trikiZenbakia = nota.data('zenbakia');
       if(norabidea == "biak"){
-        var pianoNotaItxi = e.target.dataset.itxi;
+        var pianoNotaItxi = trikiPlayer.play(trikiZenbakia,'itxi');
         $(".piano-nota[data-nota='" + pianoNotaItxi + "']").addClass(classes.colorItxi);
-          var pianoNotaIreki =  e.target.dataset.ireki;
+          var pianoNotaIreki =  trikiPlayer.play(trikiZenbakia,'ireki');
         $(".piano-nota[data-nota='" + pianoNotaIreki + "']").addClass(classes.colorIreki);
-        sounds.triki[afinazioa][trikiZenbakia].itxi.seek(0);
-        sounds.triki[afinazioa][trikiZenbakia].itxi.play();
-        sounds.triki[afinazioa][trikiZenbakia].ireki.seek(0);
-        sounds.triki[afinazioa][trikiZenbakia].ireki.play();
+
+
       } else {
-        var pianoNota = e.target.dataset[norabidea];
+        var pianoNota = trikiPlayer.play(trikiZenbakia,norabidea);
         $(".piano-nota[data-nota='" + pianoNota + "']").addClass(classes.pianoActive);
-        sounds.triki[afinazioa][trikiZenbakia][norabidea].seek(0);
-        sounds.triki[afinazioa][trikiZenbakia][norabidea].play();
+        trikiPlayer.play(trikiZenbakia,norabidea);
       }
     }
     var trikiMouseUpEv = function(e){
       var nota = $(e.target);
         nota.removeClass('color-acive');
       var trikiZenbakia = nota.data('zenbakia');
-      var notaMap = notakMap[afinazioa][trikiZenbakia];
 
+      var notaMap = trikiPlayer.getMap(afinazioa);
       if(norabidea == "biak"){
-        var pianoNotaItxi = e.target.dataset.itxi;
+        var pianoNotaItxi = trikiPlayer.stop(trikiZenbakia,'itxi');
         $(".piano-nota[data-nota='" + pianoNotaItxi + "']").removeClass(classes.colorItxi);
-        var pianoNotaIreki =  e.target.dataset.ireki;
+        var pianoNotaIreki =  trikiPlayer.stop(trikiZenbakia,'ireki');
         $(".piano-nota[data-nota='" + pianoNotaIreki + "']").removeClass(classes.colorIreki);
 
-        sounds.triki[afinazioa][trikiZenbakia].itxi.stop();
-        sounds.triki[afinazioa][trikiZenbakia].ireki.stop();
+
       } else {
-        var pianoNota = e.target.dataset[norabidea];
+        var pianoNota = trikiPlayer.stop(trikiZenbakia,norabidea);
         $(".piano-nota[data-nota='" + pianoNota + "']").removeClass(classes.pianoActive);
-        sounds.triki[afinazioa][trikiZenbakia][norabidea].stop();
+
       }
 
 
-    }
-    var loadAfinazioa = function(){
-      sounds.triki[afinazioa] = {};
-      _.forEach(notakMap[afinazioa],function(element,index){
-        var soundIreki = new Howl({
-          src: ['./samples/triki/'+ afinazioa + '/' + element.Zenbakia + ' - Ireki.ogg']
-        });
-        var soundItxi = new Howl({
-          src: ['./samples/triki/'+ afinazioa + '/' + element.Zenbakia + ' - Itxi.ogg']
-        });
-        //console.log(element);
-        sounds.triki[afinazioa][element.Zenbakia] = {
-          ireki:soundIreki,
-          itxi:soundItxi,
-        }
-        var keyItxi = note2MIDInumber(element.Itxi);
-        var keyIreki = note2MIDInumber(element.Ireki);
-        notakMap[afinazioa][element.Zenbakia].KeyItxi = keyItxi;
-        notakMap[afinazioa][element.Zenbakia].keyIreki = keyIreki;
-        $('.triki-nota[data-zenbakia=' + element.Zenbakia + ']').attr('data-keyitxi',keyItxi);
-        $('.triki-nota[data-zenbakia=' + element.Zenbakia + ']').attr('data-keyireki',keyIreki);
-        _.forEach(element,function(value,key){
-          //ponemos todos los valores de cada nota como attribute.
-          $('.triki-nota[data-zenbakia=' + element.Zenbakia + ']').attr('data-' + key.toLowerCase(),value);
-        });
-
-        });
     }
     var pianoMouseDownEv = function(e){
       var nota = $(this);
       var pianoZenbakia = nota.data('key');
       var pianoNota = nota.data('nota');
-      if($(".triki-nota[data-itxi='" + pianoNota + "']").length == 0 && $(".triki-nota[data-ireki='" + pianoNota + "']").length == 0){
+      var trikiNotak = trikiPlayer.getZenbakia(pianoNota);
+      if(trikiNotak.length === 0){
         nota.addClass(classes.pianoNone);
+      } else {
+        _.forEach(trikiNotak,function(element,index){
+          if(element.Itxi === pianoNota){
+            $(".triki-nota[data-zenbakia='" + element.Zenbakia + "']").addClass(classes.colorItxi);
+          } else if(element.Ireki === pianoNota){
+            $(".triki-nota[data-zenbakia='" + element.Zenbakia + "']").addClass(classes.colorIreki);
+          }
+        });
+
+        sounds.piano[pianoNota].seek(0);
+        sounds.piano[pianoNota].play();
       }
-      $(".triki-nota[data-itxi='" + pianoNota + "']").addClass(classes.colorItxi);
-      $(".triki-nota[data-ireki='" + pianoNota + "']").addClass(classes.colorIreki);
-      sounds.piano[pianoNota].seek(0);
-      sounds.piano[pianoNota].play();
     }
     var pianoMouseUpEv = function(e){
       var nota = $(this);
       var pianoZenbakia = nota.data('key');
       var pianoNota = nota.data('nota');
-      if($(".triki-nota[data-itxi='" + pianoNota + "']").length == 0 && $(".triki-nota[data-ireki='" + pianoNota + "']").length == 0){
+      var trikiNotak = trikiPlayer.getZenbakia(pianoNota);
+      if(trikiNotak.length === 0){
         nota.removeClass(classes.pianoNone);
+      } else {
+        _.forEach(trikiNotak,function(element,index){
+          if(element.Itxi === pianoNota){
+            $(".triki-nota[data-zenbakia='" + element.Zenbakia + "']").removeClass(classes.colorItxi);
+          } else if(element.Ireki === pianoNota){
+            $(".triki-nota[data-zenbakia='" + element.Zenbakia + "']").removeClass(classes.colorIreki);
+          }
+        });
+
+        sounds.piano[pianoNota].stop();
       }
-      $(".triki-nota[data-itxi='" + pianoNota + "']").removeClass(classes.colorItxi);
-      $(".triki-nota[data-ireki='" + pianoNota + "']").removeClass(classes.colorIreki);
-      sounds.piano[pianoNota].stop();
     }
-   var bindEvents = function(){
-     $('.triki-nota').on(evtListener[0],trikiMouseDownEv).on(evtListener[1],trikiMouseUpEv);
-     $('.piano-nota').on(evtListener[0],pianoMouseDownEv).on(evtListener[1],pianoMouseUpEv);
-     $('.btn-norabidea').click(function(){
-       if(!$(this).hasClass('active')){
-         $(".btn.active").removeClass("color-" + $(".btn.active").data('action'));
-         var action = $(this).data('action');
-         norabidea = action;
-         $(".btn").removeClass('active');
-         $(this).addClass('active').addClass('color-' + action);
-       }
-     });
+
      $('.btn-afinazioa').click(function(){
        if(!$(this).hasClass('selected')){
          $(".btn.active").removeClass("color-" + $(".btn.selected").data('action'));
@@ -186,7 +154,7 @@
          afinazioa = action;
          $(".btn").removeClass('selected');
          $(this).addClass('selected').addClass('color-' + action);
-         loadAfinazioa();
+         trikiPlayer.setAfinazioa(afinazioa);
        }
      });
      $('.piano-nota').each(function(index,el){
@@ -199,20 +167,34 @@
          sounds.piano[nota] = sound;
        });
 
-   }
-    //Cargamos el JSON
-    $.getJSON("Notak.json",function(data){
-      notakMap = data;
-      notaMapLoaded = true;
 
-      loadAfinazioa();
+   var bindEvents = function(){
+     $('.triki-nota').on(evtListener[0],trikiMouseDownEv).on(evtListener[1],trikiMouseUpEv);
+     $('.piano-nota').on(evtListener[0],pianoMouseDownEv).on(evtListener[1],pianoMouseUpEv);
+     $('.btn-norabidea').click(function(){
+       if(!$(this).hasClass('active')){
+         $(".btn.active").removeClass("color-" + $(".btn.active").data('action'));
+         var action = $(this).data('action');
+         norabidea = action;
+         $(".btn").removeClass('active');
+         $(this).addClass('active').addClass('color-' + action);
+       }
+     });
+   }
+bindEvents();
+    //Cargamos el JSON
+    $.getJSON("KeyboardMap.json",function(data){
+      keyboardMap = data;
+      keyboardMapLoaded = true;
+
+      //loadAfinazioa();
       //Bindeamos los eventos.
-      bindEvents();
+      //bindEvents();
 
 
       })
     .fail(function() {
-      console.log( "JSON error" );
+      console.log( "KeyboardMap JSON error" );
     });
 
   });
---
+++ www/js/trikiPlayer.js
@@ -0,0 +1,82 @@
+(function() {
+  var TrikiPlayer = (function() {
+    var TrikiPlayer = function(options = {},callback = function(){}) {
+      var self = this;
+      self.notakMap = false;
+      self.notaMapLoaded = false;
+      self.currentAfinazioa = "DoFa";
+      self.sounds = [];
+      self.afinazioak = [];
+      self.capitalizeFirstLetter = function(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      }
+
+
+      var loadSounds = function(){
+        //self.sounds.triki[afinazioa] = {};
+        self.sounds.triki = {};
+        _.forEach(self.notakMap,function(element,afinazioa){
+          self.sounds.triki[afinazioa] = {};
+          self.afinazioak.push(afinazioa);
+          //console.log(afinazioa);
+          _.forEach(self.notakMap[afinazioa],function(element,index){
+            var soundIreki = new Howl({
+              src: ['./samples/triki/'+ afinazioa + '/' + element.Zenbakia + ' - Ireki.ogg']
+            });
+            var soundItxi = new Howl({
+              src: ['./samples/triki/'+ afinazioa + '/' + element.Zenbakia + ' - Itxi.ogg']
+            });
+            //console.log(element);
+            self.sounds.triki[afinazioa][element.Zenbakia] = {
+              ireki:soundIreki,
+              itxi:soundItxi,
+            }
+          });
+        });
+      }
+      $.getJSON("Notak.json",function(data){
+        self.notakMap = data;
+        self.notaMapLoaded = true;
+
+        loadSounds();
+        callback();
+        })
+      .fail(function() {
+        console.log( "JSON error" );
+      });
+
+    };
+    TrikiPlayer.prototype.setAfinazioa = function setAfinazioa(afinazioa) {
+      return this.currentAfinazioa = afinazioa;
+    };
+    TrikiPlayer.prototype.getMap = function getMap(afinazioa) {
+      return this.notakMap[afinazioa];
+    };
+    TrikiPlayer.prototype.getNota = function getNota(zenbakia,norabidea) {
+      return this.notakMap[this.currentAfinazioa][zenbakia][this.capitalizeFirstLetter(norabidea)];
+    };
+    TrikiPlayer.prototype.getZenbakia = function getNota(nota) {
+      var notakItxi = _.filter(this.notakMap[this.currentAfinazioa],{'Itxi':nota});
+      var notakIreki = _.filter(this.notakMap[this.currentAfinazioa],{'Ireki':nota});
+      var notak = [].concat(notakItxi,notakIreki);
+      return notak;
+
+    };
+    TrikiPlayer.prototype.play = function play(trikiZenbakia,norabidea) {
+      this.sounds.triki[this.currentAfinazioa][trikiZenbakia][norabidea].seek(0);
+      this.sounds.triki[this.currentAfinazioa][trikiZenbakia][norabidea].play();
+      return this.notakMap[this.currentAfinazioa][trikiZenbakia][this.capitalizeFirstLetter(norabidea)];
+    };
+    TrikiPlayer.prototype.stop = function play(trikiZenbakia,norabidea) {
+      this.sounds.triki[this.currentAfinazioa][trikiZenbakia][norabidea].stop();
+      return this.notakMap[this.currentAfinazioa][trikiZenbakia][this.capitalizeFirstLetter(norabidea)];
+    };
+
+    return TrikiPlayer;
+  })();
+
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined')
+    module.exports = TrikiPlayer;
+  else
+    window.TrikiPlayer = TrikiPlayer;
+  })();
